b529199b6cee253f3410cf26a68818e5c3cd8b22
0x0nyx98 <nyxmailbox@proton.me>
2025-01-11 15:06:19 -0500

    goober was using unix pal for some reason -w-

--- library/std/src/sys/pal/mod.rs
+++ library/std/src/sys/pal/mod.rs
@@ -26,8 +26,8 @@ pub mod common;
 
 cfg_if::cfg_if! {
     if #[cfg(unix)] {
-        mod unix;
-        pub use self::unix::*;
+        //mod unix;
+        //pub use self::unix::*;
     } else if #[cfg(windows)] {
         mod windows;
         pub use self::windows::*;
